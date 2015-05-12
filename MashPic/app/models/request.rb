db9001fa92dd83a295f225515c433d8a8dfcd4c5
Matt Wuertz <mattwuertz@gmail.com>
2015-05-12 19:59:34 -0400

    beggining to move image elements

--- MashPic/app/models/request.rb
+++ MashPic/app/models/request.rb
@@ -1,10 +1,14 @@
 class Request < ActiveRecord::Base
 
   def self.search(input, labe)
+    FlickRaw.api_key= "af6cb124a512efe6089bfbaaf29669ac"
+    FlickRaw.shared_secret= "ae170a93938eaded"
 
       list   = flickr.photos.search(:tags => input)
 
-      id  = list[0].id
+      numb = rand(0..20)
+      
+      id  = list[numb].id
 
       sizes = flickr.photos.getSizes :photo_id => id
 
