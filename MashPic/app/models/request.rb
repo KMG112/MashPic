4ae0e2f3bdd1e175548cd466093d3ba5701e8297
Matt Wuertz <mattwuertz@gmail.com>
2015-05-12 10:30:49 -0400

    before flickraw

--- MashPic/app/models/request.rb
+++ MashPic/app/models/request.rb
@@ -1,2 +1,13 @@
 class Request < ActiveRecord::Base
+  include HTTParty
+
+  # def self.search(keyword)
+  # response = HTTParty.get("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+  #   :headers =>{
+  #       "tags" => keyword,
+  #       "format" => "json"
+  #   })
+  #
+  #
+  # end
 end
