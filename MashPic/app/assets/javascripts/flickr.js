3f923277aed6b18c036f161ddc42df32f3dd07eb
KMG112 <mattwuertz@gmail.com>
2015-05-13 11:41:30 -0400

    added heroku

--- MashPic/app/assets/javascripts/flickr.js
+++ MashPic/app/assets/javascripts/flickr.js
@@ -1,23 +1,5 @@
 $(function(){
 
-    //Append an empty grid with id="pics" to the gallery page content.
-
-
-    //attach search button click handler
-//   function(event) {
-//       console.log("yo")
-//       var key1 = $("#request_keyword1").val()
-//
-//         //query the Flickr API
-//         $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
-//         {
-//             tags: key1,
-//             format: "json"
-//         }).done(function(data){return data.items[0].media.m });
-//
-//
-//     });
-//
 
 $(".keyword1").css("background-image", "url("+$(".keyword1").text()+")")
 
