296787296e65d7b2c6958138f80d03783cb7b2eb
KMG112 <mattwuertz@gmail.com>
2015-05-13 13:48:19 -0400

    deleted repeat

--- MashPic/app/views/requests/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @request, :id, :keyword, :created_at, :updated_at
--- MashPic/db/migrate/20150511141104_create_requests.rb
+++
@@ -1,9 +0,0 @@
-class CreateRequests < ActiveRecord::Migration
-  def change
-    create_table :requests do |t|
-      t.string :keyword1
-      t.string :keyword2
-      t.string :keyword3
-    end
-  end
-end
