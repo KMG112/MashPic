8e8bac1ba94ab06453a3e3ea914b2670fc4c6b5c
KMG112 <mattwuertz@gmail.com>
2015-06-18 17:06:21 -0400

    added clipArt to aws s3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
-//= require jquery-ui/resizable
-//= require jquery-ui/draggable
+//= require jquery-ui
 //= require_tree .
 
--- app/assets/javascripts/flickr.js
+++ app/assets/javascripts/flickr.js
@@ -1,15 +1,15 @@
 $(function(){
 
 
-$(".keyword1").css("background-image", "url("+$("div.keyword1").attr('value')+")")
+$(".keyword1").css("background-image", "url("+$("div.keyword1").attr('value')+")");
 
 
-$(".keyword3").css("background-image", "url("+$("div.keyword3").attr('value')+")")
+$(".keyword3").css("background-image", "url("+$("div.keyword3").attr('value')+")");
 
 
-$(".image").css("background-image", "url('https://s3.amazonaws.com/mashpic/collage.png')")
+$(".image").css("background-image", "url('https://s3.amazonaws.com/mashpic/collage.png')");
 
-$('#clipArt').draggable();
-$('#clipArt').resizable();
+$('#clipArtHolder').draggable();
+$('img').resizable();
 });
 
